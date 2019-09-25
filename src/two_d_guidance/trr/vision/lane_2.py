080fcd8567e136f1d429d0cf3c201f33b3aa0d14
poine <poinix@gmail.com>
2019-09-25 07:29:12 +0200

    Fix points above horizon projected behind

--- src/two_d_guidance/trr/vision/lane_2.py
+++ src/two_d_guidance/trr/vision/lane_2.py
@@ -61,7 +61,8 @@ class Contour2Pipeline(trr_vu.Pipeline):
             cnt_be = self.bird_eye.points_imp_to_be(cnt_imp)
             self.cnts_be.append(cnt_be)
             cnt_lfp = self.bird_eye.points_imp_to_blf(cnt_imp)
-            np.clip(cnt_lfp[:, 0, 0], 0.01, 3., out=cnt_lfp[:, 0, 0])
+            cnt_lfp[cnt_lfp[:, 0, 0] < 0.01] = 20
+            np.clip(cnt_lfp[:, 0, 0], None, 3., out=cnt_lfp[:, 0, 0])
             self.cnts_lfp.append(cnt_lfp)
         self.cnts_lfp = np.array(self.cnts_lfp)
 
